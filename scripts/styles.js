444fd3fb05d1fb1f96d0bd0e025039b36238dd38
Ace Of Spades <48494858+Phoenix-AOS@users.noreply.github.com>
2020-12-14 11:50:09 -0500

    Working on themes...

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,19 +1,16 @@
 // DOM Elements
 
-const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
-const solarButton = document.getElementById('solar');
+const darkButton = document.getElementById('darkmode');
+const lightButton = document.getElementById('lightmode');
 const body = document.body;
 
 
 // Apply the cached theme on reload
 
 const theme = localStorage.getItem('theme');
-const isSolar = localStorage.getItem('isSolar');
 
 if (theme) {
   body.classList.add(theme);
-  isSolar && body.classList.add('solar');
 }
 
 // Button Event Handlers
@@ -25,32 +22,5 @@ darkButton.onclick = () => {
 
 lightButton.onclick = () => {
   body.classList.replace('dark', 'light');
-
   localStorage.setItem('theme', 'light');
-};
-
-solarButton.onclick = () => {
-
-  if (body.classList.contains('solar')) {
-    
-    body.classList.remove('solar');
-    solarButton.style.cssText = `
-      --bg-solar: var(--yellow);
-    `
-
-    solarButton.innerText = 'solarize';
-
-    localStorage.removeItem('isSolar');
-
-  } else {
-
-    solarButton.style.cssText = `
-      --bg-solar: white;
-    `
-
-    body.classList.add('solar');
-    solarButton.innerText = 'normalize';
-
-    localStorage.setItem('isSolar', true);
-  }
 };
