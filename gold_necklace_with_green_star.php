3866a8f0d0461a3c95dc1c5884ac59cb97006f1c
ScratchCGI <75954718+ScratchCGI@users.noreply.github.com>
2020-12-14 10:47:01 -0500

    Adding initial (existing) files.

---
+++ gold_necklace_with_green_star.php
@@ -0,0 +1,29 @@
+<?php echo file_get_contents("html/portfolio_top.html"); ?>
+      <div class="column">
+        <div class="content">
+          <a href="img/gold_necklace_with_green_star/WA - Full.jpg">
+            <img src="img/gold_necklace_with_green_star/WA - Full.jpg" style="width:100%">
+          </a>
+          <h3 class="title">Wide Angle</h3>
+          <p>This was my first render using LuxCore.</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="content">
+          <a href="img/gold_necklace_with_green_star/CloseUp - Chain.jpg">
+            <img src="img/gold_necklace_with_green_star/CloseUp - Chain.jpg" style="width:100%">
+          </a>
+          <h3 class="title">Chain CloseUp</h3>
+          <p>A closeup shot of the chain.</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="content">
+          <a href="img/gold_necklace_with_green_star/CloseUp - Pendant.jpg">
+            <img src="img/gold_necklace_with_green_star/CloseUp - Pendant.jpg" style="width:100%">
+          </a>
+          <h3 class="title">Pendant CloseUp</h3>
+          <p>A closeup shot of the pendant.</p>
+        </div>
+      </div>
+<?php echo file_get_contents("html/portfolio_bottom.html"); ?>
